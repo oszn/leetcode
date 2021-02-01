bf5b253e47a4c7125a21cf2426f944344ba50d60
oszn <1091756452@qq.com>
2021-02-01 21:58:42 +0800

    day 4

--- recordLeetCode/main.cpp
+++ recordLeetCode/main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+//package
+#include<algorithm>
+//data struct
 #include<vector>
 #include<map>
 #include<queue>
@@ -6,6 +9,47 @@
 #include<tuple>
 
 using namespace std;
+
+
+class unionfind {
+public:
+	vector<int> record;
+	vector<int> size;
+	int area;
+	unionfind(int len) {
+		area = len;
+		for (int i = 0; i < len; i++) {
+			record.push_back(i);
+			size.push_back(1);
+		}
+	}
+	int find(int x) {
+		if (x == record[x])
+			return x;
+		return find(record[x]);
+	}
+	bool un_ion(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x == y)
+			return false;
+		if (size[x] < size[y]) {
+			swap(x, y);
+		}
+		area--;
+		size[x] = size[x] + size[y];
+		record[y] = x;
+		return true;
+	}
+	bool connect(int x, int y) {
+		x = find(x);
+		y = find(y);
+		return x == y;
+	}
+};
+
+
+
 
 namespace lt1631 {
 	vector<int> arr;
@@ -70,7 +114,80 @@ namespace lt1631 {
 
 	}
 }
+namespace lt778{
+	//778. 水位上升的泳池中游泳
+	int swimInWater(vector<vector<int>>& grid) {
+		if (grid.empty() || grid[0].empty())
+			return 0;
+		vector<tuple<int, int, int>> cmap;
+		int cow = grid.size(), row = grid[0].size();
+		for (int i = 0; i < cow; i++) {
+			for (int j = 0; j < row; j++) {
+				int id = i * row + j;
+				if (i > 0) {
+					cmap.emplace_back(id, id - row, max(grid[i][j], grid[i - 1][j]));
+				}
+				if (j > 0) {
+					cmap.emplace_back(id, id - 1, max(grid[i][j], grid[i][j - 1]));
+				}
+			}
+		}
+		sort(cmap.begin(), cmap.end(), [](const auto&e1, const auto&e2) {
+			auto&&[x1, y1, v1] = e1;
+			auto&&[x2, y2, v2] = e2;
+			return v1 < v2;
+		});
+		unionfind f(cow*row);
+		for (const auto e : cmap) {
+			int x = get<0>(e);
+			int y = get<1>(e);
+			int val = get<2>(e);
+			f.un_ion(x, y);
+			if (f.connect(0, cow*row-1))
+				return val;
+			
+		}
+		return 0;
+	}
+}
+namespace lt839 {
+	// 839. 相似字符串组
+	bool similar(string&s1,string&s2) {
+		int count = 0;
+		int index1 = -1, index2 = -1;
+		if (s1.size() != s2.size())
+			return false;
+		for (int i = 0; i < s1.size(); i++) {
+			if (s1[i] != s2[i]) {
+				if (count == 2)
+					return false;
+				if (count == 0)
+					index1 = i;
+				else if (count == 1)
+					index2 = i;
+				count++;
+			}
+		}
+		if (count == 1)
+			return false;
 
+		if (count==0||(count==2&&s1[index1] == s2[index2] && s1[index2] == s2[index1]))
+			return true;
+		return false;
+	}
+	int numSimilarGroups(vector<string>& strs) {
+
+		unionfind f(strs.size());
+		for (int i = 0; i < strs.size();i++) {
+			for (int j = i + 1; j < strs.size(); j++) {
+				if (!f.connect(i, j)&&similar(strs[i],strs[j])) {
+					f.un_ion(i, j);
+				}
+			}
+		}
+		return f.area;
+	}
+}
 int main() {
 
 }
